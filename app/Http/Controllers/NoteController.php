5ca37c8545459c98bdcf547d9b4e193d86d99570
edriso <edriso.dev@gmail.com>
2023-04-10 16:30:22 +0200

    add note routes

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    public function index()
+    {
+        return "hello";
+    }
+
+    public function create()
+    {
+        return "hello";
+    }
+
+    public function store()
+    {
+        return "hello";
+    }
+
+    public function show()
+    {
+        return "hello";
+    }
+
+    public function edit()
+    {
+        return "hello";
+    }
+
+    public function update()
+    {
+        return "hello";
+    }
+
+    public function destroy()
+    {
+        return "hello";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
+Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
+Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::get('/notes/{note}', [NoteController::class, 'show'])->name('notes.show');
+Route::get('/notes/{note}/edit', [NoteController::class, 'edit'])->name('notes.edit');
+Route::patch('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
+
+require __DIR__ . '/auth.php';
