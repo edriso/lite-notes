d7e093bc0ac31aa7bb37e7c6bb3567fd7b9d87b7
edriso <edriso.dev@gmail.com>
2023-04-10 18:18:56 +0200

    remove empty line

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -18,7 +18,6 @@
                     </span>
                 </div>
             @empty
-
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         {{ __('You have not taken notes yet!') }}
