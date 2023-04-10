ca2de7eeda5587cf87d46a15ad7984b7b37e39bb
edriso <edriso.dev@gmail.com>
2023-04-10 13:37:29 +0200

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<x-base>
+
+    <x-slot name="user">Disso</x-slot>
+
+    <x-slot name="content">
+        <h1 class="text-4xl font-bold">About</h1>
+        <p class="mt-4 text-lg">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam eius iusto in soluta harum vero dicta quas
+            doloremque veniam a repudiandae, laboriosam, recusandae quidem consequuntur reprehenderit sint perferendis
+            maxime. Recusandae temporibus dignissimos officia blanditiis veniam sed quod provident ad saepe!
+        </p>
+    </x-slot>
+
+</x-base>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('user', 'Disso')
+
+@section('content')
+<h1 class="text-4xl font-bold">Homepage</h1>
+<p class="mt-4 text-lg">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam eius iusto in soluta harum vero dicta quas doloremque veniam a repudiandae, laboriosam, recusandae quidem consequuntur reprehenderit sint perferendis maxime. Recusandae temporibus dignissimos officia blanditiis veniam sed quod provident ad saepe!
+</p>
+@endsection
