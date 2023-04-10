43202a23c5757d6218ceb377dfb72168bc01abad
edriso <edriso.dev@gmail.com>
2023-04-10 13:45:36 +0200

    change app title dynamically from env file

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lite Notes</title>
+    <title>{{ config('app.name') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
     <nav class="bg-gray-100 px-8 py-4 text-gray-700 flex items-center justify-between">
         <a class="font-bold text-2xl" href="/">Lite Notes</a>
@@ -17,4 +19,5 @@
         @yield('content')
     </main>
 </body>
+
 </html>
