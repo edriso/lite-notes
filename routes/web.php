eb7d42c6c09bb1487d79978e1a39e36a2346779e
edriso <edriso.dev@gmail.com>
2023-04-10 14:38:49 +0200

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [WelcomeController::class, 'index']);
-
-Route::get('/home', function () {
-    return view('home');
-});
-
-Route::get('/about', function () {
-    return view('about');
+Route::get('/', function () {
+    return view('welcome');
 });
