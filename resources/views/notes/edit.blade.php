34fb290f5efa725df726a47e01bc2874da93bf3e
edriso <edriso.dev@gmail.com>
2023-04-11 12:45:29 +0200

    lowercase 'post'

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <form method="POST" action="{{ route('notes.update', $note) }}">
-                        @method('PATCH')
+                    <form method="post" action="{{ route('notes.update', $note) }}">
+                        @method('patch')
                         @csrf
                         <x-text-input field="title" :value="@old('title', $note->title)" type="text" name="title" class="w-full"
                             placeholder="Title" />
